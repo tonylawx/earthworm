a05279b507ac64d228519f8b4da7981174175ed6
tonylawx <xtonylaw@gmail.com>
2024-03-29 01:45:52 +0800

    feat: 数据库增加字段存储github用户信息

--- apps/api/src/user/model/user.dto.ts
+++ apps/api/src/user/model/user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, Length } from 'class-validator';
+import { IsNotEmpty, Length, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
@@ -16,6 +16,22 @@ export class CreateUserDto {
   @IsNotEmpty({ message: '密码不能为空' })
   @Length(6, 20, { message: '密码长度为6-20位' })
   password: string;
+
+  @ApiProperty()
+  @IsString({ message: 'GitHub访问令牌必须是一个字符串' })
+  accessToken: string; // 新增GitHub访问令牌
+
+  @ApiProperty()
+  @IsString({ message: 'GitHub刷新令牌必须是一个字符串' })
+  refreshToken: string; // 新增GitHub刷新令牌
+
+  @ApiProperty()
+  @IsString({ message: 'GitHub用户ID必须是一个字符串' })
+  githubId: string; // 新增GitHub用户ID
+
+  @ApiProperty()
+  @IsString({ message: 'GitHub用户名必须是一个字符串' })
+  githubUsername: string; // 新增GitHub用户名
 }
 
 export class FindUserDto {
@@ -23,4 +39,8 @@ export class FindUserDto {
   @IsNotEmpty({ message: '手机号码不能为空' })
   @Length(6, 20, { message: '手机号码长度应在6到20位之间' })
   phone: string;
+
+  @ApiProperty()
+  @IsString({ message: 'GitHub用户ID必须是一个字符串' })
+  githubId: string; // 新增可选的GitHub用户ID
 }
--- packages/schema/src/schema/user.ts
+++ packages/schema/src/schema/user.ts
@@ -13,5 +13,8 @@ export const user = mysqlTable("users", {
   password: text("password").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").onUpdateNow(),
-  
+  githubId: varchar("github_id", { length: 255 }), // 存储GitHub用户ID
+  githubAccessToken: text("github_access_token"), // 存储GitHub访问令牌
+  githubRefreshToken: text("github_refresh_token"), // 存储GitHub刷新令牌
+  githubUsername: varchar("github_username", { length: 255 }), // 存储GitHub用户名
 });
